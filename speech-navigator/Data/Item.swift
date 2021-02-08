a1929f1bcd7d95d394b3748310bc0457d174ca88
Ken Dong <yid164@mail.usask.ca>
2021-02-07 18:26:11 -0600

    basically done the sample ui of oeprations

--- speech-navigator/Data/Item.swift
+++ speech-navigator/Data/Item.swift
@@ -6,9 +6,37 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct Item: Identifiable, Hashable {
+class Item: Identifiable, Hashable {
     let id: UUID
-    let number: Double
-    let unit: Unit
+    var number: Double
+    var unit: Unit
+    
+    init(number: Double, unit: Unit) {
+        self.id = UUID()
+        self.number = number
+        self.unit = unit
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: Item, rhs: Item) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+}
+
+extension Item {
+    var itemValue: Binding<String> {
+        Binding<String>(
+            get: { "\(self.number) \(self.unit.symbol)" },
+            set: {
+                self.number = Double($0.filter { $0.isNumber }) ?? 0
+                self.unit = Unit(symbol: $0.filter { $0.isLetter })
+            }
+        )
+    }
 }
--- speech-navigator/View/OperationsView.swift
+++ speech-navigator/View/OperationsView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct OperationsView: View {
-    @State var item1: String = ""
-    @State var item2: String = ""
+    @State var selectedItem: Item? = nil
+
+    @State var items: [Item] = [
+        Item(number: 13, unit: Unit(symbol: "g")),
+        Item(number: 14, unit: Unit(symbol: "g")),
+        Item(number: 15, unit: Unit(symbol: "g")),
+        Item(number: 16, unit: Unit(symbol: "g")),
+        Item(number: 17, unit: Unit(symbol: "g"))
+        
+    ]
     
     var body: some View {
         Form {
@@ -17,12 +25,14 @@ struct OperationsView: View {
                 Text("Start Your Operations")
             }
             
-            Section(header: Text("Item 1")) {
-                TextField("The first item", text: $item1)
-            }
-            
-            Section(header: Text("Item 2")) {
-                TextField("The second item", text: $item2)
+            ForEach(0 ..< items.count) { index in
+                Section(header: Text("Item \(index)")) {
+                    TextField("Record your item \(index)", text: items[index].itemValue )
+                }
+                .listRowBackground(Color( selectedItem == items[index] ? .blue : .systemBackground))
+                .onTapGesture {
+                    self.selectedItem = items[index]
+                }
             }
         }
     }
